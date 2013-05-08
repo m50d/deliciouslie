45020eeef597922638db4809b53335c875353ada
Michael Donaghy <md401@srcf.ucam.org>
2013-05-08 23:08:49 +0100

    The application context object
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -1,9 +1,11 @@
 package net.homelinux.md401.deliciouslie
 
 import shapeless.HList
+import shapeless.Selector
 
 trait Context[Deps <: HList] {
-  def fromContext[A]: A = null.asInstanceOf[A]
+  def deps: Deps
+  def fromContext[A]()(implicit selector: Selector[Deps, A]): A = deps.select[A]
 }
 
 trait Component[Deps <: HList] {
