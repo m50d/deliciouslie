93408541704ce12134bf19f285861cb51e0abbf1
Michael Donaghy <md401@srcf.ucam.org>
2013-05-08 23:02:49 +0100

    An idea of a piece I'll want
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -2,6 +2,10 @@ package net.homelinux.md401.deliciouslie
 
 import shapeless.HList
 
+trait Context[Deps <: HList] {
+  def fromContext[A]: A = null.asInstanceOf[A]
+}
+
 trait Component[Deps <: HList] {
 
 }
