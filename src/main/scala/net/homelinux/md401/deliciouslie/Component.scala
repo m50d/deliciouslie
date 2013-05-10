7ae1a45255226f35996a3e69dc17a80953b04027
Michael Donaghy <md401@srcf.ucam.org>
2013-05-10 07:55:59 +0100

    A possibly useful signature

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -1,7 +1,6 @@
 package net.homelinux.md401.deliciouslie
 
-import shapeless.HList
-import shapeless.Selector
+import shapeless._
 
 trait Context[Deps <: HList] {
   def deps: Deps
@@ -14,4 +13,8 @@ trait Component[Deps <: HList] {
   val cake = new Object(){
     def foreach[A](f: Context[Deps] => A): ContextDependent[Deps, A] = ContextDependent(f)
   }
+}
+
+trait ComponentImpl[A] {
+  def withComponent[B, PD <: HList](f: A :: PD => B): PD => B
 }
