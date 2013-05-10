ac819df84f067d19ee4423346682bdd20b919ed7
Michael Donaghy <md401@srcf.ucam.org>
2013-05-10 08:03:00 +0100

    Let's try this

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -2,16 +2,12 @@ package net.homelinux.md401.deliciouslie
 
 import shapeless._
 
-trait Context[Deps <: HList] {
-  def deps: Deps
-  def inject[A]()(implicit selector: Selector[Deps, A]): A = deps.select[A]
-}
-
-case class ContextDependent[Deps <: HList, A](f: Context[Deps] => A)
+case class ContextDependent[Deps <: HList, A](f: Deps => A)
 
 trait Component[Deps <: HList] {
-  val cake = new Object(){
-    def foreach[A](f: Context[Deps] => A): ContextDependent[Deps, A] = ContextDependent(f)
+  val context = new Object(){
+    def foreach[A, B](f: A => B)(implicit selector: Selector[Deps, A]): ContextDependent[Deps, B] =
+      ContextDependent({deps => f(deps.select[A])})
   }
 }
 
