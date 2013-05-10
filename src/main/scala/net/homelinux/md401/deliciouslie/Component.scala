9aed7aa1bc288be0daffbdea811f019cc34e48da
Michael Donaghy <md401@srcf.ucam.org>
2013-05-10 08:10:06 +0100

    Uncomfortable edge case

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -4,6 +4,11 @@ import shapeless._
 
 case class ContextDependent[Deps <: HList, A](f: Deps => A)
 
+object Implicits {
+  implicit def noContext2Function[A](cd: ContextDependent[HNil, A]): () => A
+   = () => cd.f(HNil)
+}
+
 trait Component[Deps <: HList] {
   val context = new Object(){
     def foreach[A, B](f: A => B)(implicit selector: Selector[Deps, A]): ContextDependent[Deps, B] =
