9fc32252e7e32e2aabd7a0cb27835af9859f3ebe
Michael Donaghy <md401@srcf.ucam.org>
2013-05-11 21:11:08 +0100

    Almost right, but we want to step it up a level
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/test/scala/net/homelinux/md401/deliciouslie/DeliciousLieTest.scala
+++ src/test/scala/net/homelinux/md401/deliciouslie/DeliciousLieTest.scala
@@ -14,35 +14,33 @@ class DeliciousLieTest {
     def stop(): Unit
   }
   val lifecycle = createMock(classOf[Service1Lifecycle])
-  
+
   val bakedNil = BakedNil()
   val rawComponent1 = new Layer[HNil, Service1] {
     val withLayer = {
       hn: HNil =>
-        new BakedLayer[Service1](){
-          def withComponent(f: Service1 => Unit) = {
-            lifecycle.start()
-            f(Service1())
-            lifecycle.stop()
-          }
+        for { f <- callback } yield {
+          lifecycle.start()
+          f(Service1())
+          lifecycle.stop()
         }
     }
   }
   val rawComponent2 = new Layer[Service1 :: HNil, Service2] {
     val withLayer = {
-      l : (Service1 :: HNil) =>
-        new BakedLayer[Service2](){
+      l: (Service1 :: HNil) =>
+        new BakedLayer[Service2]() {
           def withComponent(f: Service2 => Unit) = {
             f(Service2(l.head))
           }
         }
     }
   }
-  
+
   val rawComponent3 = new Layer[Service2 :: HNil, Service3] {
     val withLayer = {
-      l : (Service2 :: HNil) =>
-        new BakedLayer[Service3](){
+      l: (Service2 :: HNil) =>
+        new BakedLayer[Service3]() {
           def withComponent(f: Service3 => Unit) = {
             f(Service3(l.head))
           }
@@ -50,7 +48,7 @@ class DeliciousLieTest {
     }
   }
   val cake = bakedNil wit rawComponent1 wit rawComponent2 wit rawComponent3
-  
+
   @Test
   def instantiatesComponentExactlyOnce() {
     lifecycle.start()
@@ -58,7 +56,7 @@ class DeliciousLieTest {
     lifecycle.stop()
     expectLastCall()
     replay(lifecycle)
-    cake.burn({l => {}})
+    cake.burn({ l => {} })
     verify(lifecycle)
   }
 }
