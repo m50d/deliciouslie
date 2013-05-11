e6a0f3d8abd412a21b778db392c835a4d9254407
Michael Donaghy <md401@srcf.ucam.org>
2013-05-11 16:56:26 +0100

    In some way the most crucial part of the enterprise - I'm actually starting to think this might all be possible
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/net/homelinux/md401/deliciouslie/DeliciousLie.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/DeliciousLie.scala
@@ -6,17 +6,26 @@ import TypeOperators._
 import HList._
 import BasisConstraint._
 
-object DeliciousLie {
-	
-}
-
 trait Layer[A] {
   def withLayer[B](f: A => B): B
 }
 
+case class ContextDependent[Deps <: HList, A](f: Deps => A)
+
+trait RawLayer[Deps <: HList, A] {
+  def withLayer: ContextDependent[Deps, Layer[A]]
+}
+
 class Cake[Layers <: HList: *->*[Layer]#λ](layers: Layers) {
-  def withCake[RequiredLayers <: HList, B](f: RequiredLayers => B)(implicit removeAll: RemoveAll[RequiredLayers,Layers]): B = {
+  def withCake[RequiredLayers <: HList, A](f: RequiredLayers => A)(implicit removeAll: RemoveAll[RequiredLayers,Layers]): A = {
     val (requiredLayers, otherLayers) = layers.removeAll[RequiredLayers]
     f(requiredLayers)
   }
+  def bakeLayer[RequiredLayers <: HList, A](rawLayer: RawLayer[RequiredLayers, A])(implicit removeAll: RemoveAll[RequiredLayers,Layers]): Cake[Layer[A] :: Layers] = {
+	val bakedLayer: Layer[A] = withCake(rawLayer.withLayer.f)
+	new Cake(bakedLayer :: layers)
+  }
+}
+
+object DeliciousLie {
 }
