ae66c6e0f0d08113ae0f31f6ff05d071ca0147a5
Michael Donaghy <md401@srcf.ucam.org>
2013-05-11 08:04:42 +0100

    The easy monad

--- src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
+++ src/main/scala/net/homelinux/md401/deliciouslie/Component.scala
@@ -1,6 +1,8 @@
 package net.homelinux.md401.deliciouslie
 
 import shapeless._
+import scalaz._
+import Scalaz._
 
 case class ContextDependent[Deps <: HList, A](f: Deps => A)
 
@@ -12,6 +14,12 @@ object Implicits {
 //  	(implicit prepend: Prepend[Deps1, Deps2]): ContextDependent[prepend.Out, A] =
 //    ContextDependent({deps: prepend.Out => cd.f(deps.take(Deps1.length)).f(deps.drop(Deps1.length))})
   def instantiate[A](component: ComponentImpl[A, HNil]) = {}
+  class ContextDependentMonad[Deps <: HList] extends Monad[({type cd[A] = ContextDependent[Deps, A]})#cd] {
+    def point[A](f: => A) = ContextDependent({deps => f})
+    def bind[A, B](fa: ContextDependent[Deps, A])(f: A => ContextDependent[Deps, B]) =
+      ContextDependent({deps: Deps => 
+        f(fa.f(deps)).f(deps)})
+  }
 }
 
 trait Component[Deps <: HList] {
